01a7654a9c0a9728ba0b92a30af81be13488d176
Abdullateef Jokanola <ajokanola@parkway.ng>
2026-08-05 17:37:58 +0100

    Add project files.

---
+++ SignalR_Demo_Project/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SignalR_Demo_Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
